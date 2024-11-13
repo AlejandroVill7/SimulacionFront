01dc453b70ef1de674726574a9568b2f9b08b917
Alejandro <alejandrozzzxx@gmail.com>
2024-11-13 11:30:48 -0600

    adding files css html js

---
+++ Script.js
@@ -0,0 +1,9 @@
+// Función al hacer clic en el botón CONSULTAR
+document.getElementById("consultarBoton").addEventListener("click", function() {
+    alert("Consulta en proceso...");
+});
+
+// Función al hacer clic en el botón LEER MÁS
+document.getElementById("leerMasBoton").addEventListener("click", function() {
+    alert("Mostrando más información...");
+});
